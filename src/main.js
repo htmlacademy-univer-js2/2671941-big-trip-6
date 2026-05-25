891adbedf9595e343a97c336b7a846710632c0bd
karpenkova123 <v.a.karpenkova@urfu.ru>
2026-05-25 18:53:14 +0500

    Change to module2-task1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import { HeaderPresenter, MainPresenter } from './presenter/presenter.js';
+import TripPresenter from './presenter/presenter.js';
+import PointsModel from './model/points-model.js'; // Убедитесь, что путь правильный
 
 const headerContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
 
-const headerPresenter = new HeaderPresenter({ container: headerContainer });
-const mainPresenter = new MainPresenter({ container: eventsContainer });
+// Создаем экземпляр модели
+const pointsModel = new PointsModel();
 
-headerPresenter.init();
-mainPresenter.init();
+const presenter = new TripPresenter({
+headerContainer: headerContainer,
+  eventsContainer: eventsContainer,
+  pointsModel: pointsModel // Добавляем модель в презентер
+});
+
+presenter.init();
